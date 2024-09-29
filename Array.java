86743bd7d1e1374baa95de94439caee69cf6de7e
louisderex <150283969+louisderex@users.noreply.github.com>
2024-09-30 02:23:16 +0300

    Delete Array.java

--- Array.java
+++
@@ -1,8 +0,0 @@
-public class Array {
-    public static void main(String[] args){
-        int[] numbers = {10, 20, 30, 40, 50, 60};
-        //accessing an array using for loop
-        for(int i = 0; i < numbers, numbers.length i++);
-        System.out.println("Element at index" + "i" + "numbers[i]");
-    }
-}
